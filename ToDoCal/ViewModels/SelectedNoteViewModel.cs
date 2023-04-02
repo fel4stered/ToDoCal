a1e8775040eba574ace85171e3cab2c45677cc0c
foriior@mail.ru <foriior@mail.ru>
2023-04-02 15:37:42 +0500

    UPD

--- ToDoCal/ViewModels/SelectedNoteViewModel.cs
+++ ToDoCal/ViewModels/SelectedNoteViewModel.cs
@@ -8,5 +8,13 @@ namespace ToDoCal.ViewModels
 {
     public class SelectedNoteViewModel
     {
+        public string DisplayTitle { get; set; }
+        public string DisplayDate { get; set;  }
+        public string DisplayStatus { get; set; }
+        public string DisplayIsTask { get; set; }
+        
+        
+        public string DisplayText { get; set; }
+
     }
 }
